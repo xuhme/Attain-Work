34545304d28e0c1b1c4d20413078ed1fe4235146
xuhme <yeans201@gmail.com>
2023-05-09 06:21:53 +0700

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,8 +23,7 @@ const Hero = () => {
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
           Attain Work Co Ltd. was established in 2021, and is now a leading
           provider of network devices, maintenance, and support services.The
-          company provides reliable and secure networking solutions, including
-          trusted router and switch brands.
+          company provides reliable and secure networking solutions,
         </p>
       </div>
 
