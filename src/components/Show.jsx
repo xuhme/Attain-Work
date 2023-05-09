f26e586a1af94f506adcbcae21ff6b30a038d701
xuhme <yeans201@gmail.com>
2023-05-09 14:34:25 +0700

    Update Show.jsx

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -8,7 +8,7 @@ const Show = () => (
         key={show.id}
         className={`flex-1 flex justify-start items-center flex-row m-3 sm:ml-0 ml-10 `}
       >
-        <h4 className="font-poppins font-semibold xs:text-[30px] text-[20px] xs:leading-[54px] leading-[44px] text-gradient uppercase ml-3">
+        <h4 className="font-poppins font-semibold xs:text-[24px] text-[16px] xs:leading-[54px] leading-[44px] text-gradient uppercase ml-3">
           {show.title}
         </h4>
       </div>
